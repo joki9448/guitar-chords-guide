616123bc83ac9bb1a228763db125996b17fd7ff8
John Kim <joki9448@gmail.com>
2023-12-30 14:22:10 -0500

    created Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <ins class="scales_chords_api" chord="D#m(maj9)"></ins>
-
+      <Header/>
     </div>
   );
 }
 
 export default App;
+
