4d42c64db6fd610af0de882c2d787868bafb79cd
John Kim <joki9448@gmail.com>
2024-01-03 11:22:00 -0500

    set up base search bar form submit

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {useState} from 'react';
 
 function SearchBar() {
+    const [chord, setChord] = useState('');
+    const [chordSubmit, setChordSubmit] = useState('');
+    console.log('chord', chord);
+    console.log('chordSubmit', chordSubmit);
+
+    const handleSubmit = () => {
+        return (
+            setChordSubmit(chord)
+        );
+    };
+
     return (
-        <form>
-            <input>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                onChange={(e) => setChord(e.target.value)}
+                value={chord}
+                placeholder="Enter Chord"
+            >
             </input>
+            <button
+                className="search-btn"
+                type="button"
+                onClick={(e) => setChordSubmit(chord)}
+            >
+                Search
+            </button>
         </form>
     );
 };
