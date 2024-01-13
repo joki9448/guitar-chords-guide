360314ca234bd2fc73512867ae483d32825c09ac
John Kim <joki9448@gmail.com>
2024-01-13 17:24:19 -0500

    added frets to fretboard

--- src/components/Fretboard.js
+++ src/components/Fretboard.js
@@ -1,3 +1,15 @@
-function Fretboard() {return (<div className="fretboard"></div>)}
+function Fretboard() {
+    const fretTotal = 6;
+    const frets = [];
+
+    for (let i = 1; i <= fretTotal; i++) {
+        frets.push(<div className={`fret-${i}`}></div>);
+    }
+    return (
+        <div className="fretboard">
+            {frets}
+        </div>
+    )
+}
 
 export default Fretboard;
